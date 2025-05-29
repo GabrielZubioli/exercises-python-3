96a8c0f8ac6c9801822e0f431000440116ae2809
GabrielZubioli <zubioligabriel@gmail.com>
2025-05-28 21:58:54 -0300

    first commit

---
+++ exer1.py
@@ -0,0 +1,5 @@
+real = float(input("Insira um valo em real: "))
+cotacao = float(input("Insira o valor da cotacao do dollar: "))
+conversao = real * cotacao
+
+print("O valor da conversão de real pra dollar foi:",conversao)
